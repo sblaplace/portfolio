29ba39fea48558d6dc41181db706150c3475eb21
Cactus <sblaplace@gmail.com>
2022-08-06 15:29:57 +0200

    Add bundle analyzer, image resizing

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,9 @@ export async function getStaticProps() {
   };
 }
 
+const postImageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`
+}
 
 
 export default function Home({ posts }) {
@@ -42,6 +45,7 @@ export default function Home({ posts }) {
                 height={340}
                 alt={frontmatter.title}
                 src={`/${frontmatter.socialImage}`}
+                loader={postImageLoader}
               />
               <h1 className='p-4'>{frontmatter.title}</h1>
             </a>

cf0c81424b967729f978ca9d6e9742812c46eb8f
Cactus <sblaplace@gmail.com>
2022-08-11 12:35:53 +0200

    Make menu responsive

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,8 @@
 import { forwardRef } from 'react'
 import Link from 'next/link'
 import { Menu } from '@headlessui/react'
+import { useMediaQuery } from 'react-responsive';
+
 
 const MyLink = forwardRef((props, ref) => {
     let { href, children, ...rest } = props
@@ -14,6 +16,8 @@ const MyLink = forwardRef((props, ref) => {
 })
 
 export default function Layout({ children }) {
+    const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
     return (
         <div className='flex flex-col min-h-screen bg-forestghosts text-zinc-600 bg-background-pattern flex justify-center'>
             <header className='m-4 bg-cambridgeblue bg-background-pattern mb-8 py-4'>
@@ -22,24 +26,40 @@ export default function Layout({ children }) {
                         <MyLink className={'pl-8'} href='/'>
                             HOME
                         </MyLink>
-                        <Menu.Button className={`tracking-widest pl-12`}>MORE</Menu.Button>
-                        <Menu.Items className="absolute mt-2 w-56 origin-top-right bg-cambridgeblue bg-background-pattern focus:outline-none z-10">
-                            <Menu.Item>
-                                <MyLink href='/projects' className="group flex w-full items-center px-2 py-8">
+                        {isMobile ?
+                            <>
+                                <Menu.Button className={`tracking-widest pl-12`}>MORE</Menu.Button>
+                                <Menu.Items className="absolute mt-2 w-56 origin-top-right bg-cambridgeblue bg-background-pattern focus:outline-none z-10">
+                                    <Menu.Item>
+                                        <MyLink href='/projects' className="group flex w-full items-center px-2 py-8">
+                                            PROJECTS
+                                        </MyLink>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <MyLink href='/contract' className="group flex w-full items-center px-2 py-8">
+                                            CONTRACT
+                                        </MyLink>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <MyLink href='/about' className="group flex w-full items-center px-2 py-8">
+                                            ABOUT
+                                        </MyLink>
+                                    </Menu.Item>
+                                </Menu.Items>
+                            </> :
+                            <>
+                                <MyLink className={'pl-8'} href='/projects'>
                                     PROJECTS
                                 </MyLink>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <MyLink href='/contract' className="group flex w-full items-center px-2 py-8">
+                                <MyLink className={'pl-8'} href='/contract'>
                                     CONTRACT
                                 </MyLink>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <MyLink href='/about' className="group flex w-full items-center px-2 py-8">
+                                <MyLink className={'pl-8'} href='/about'>
                                     ABOUT
                                 </MyLink>
-                            </Menu.Item>
-                        </Menu.Items>
+                            </>
+                        }
+
                     </Menu>
                 </div>
             </header>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import fs from 'fs';
 import matter from 'gray-matter';
 import Link from 'next/link';
